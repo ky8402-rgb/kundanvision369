39d0da51f41bbe7fa47af8f71c9a3b24787fa8f0
ky8402-rgb <ky8402@gmail.com>
2026-08-31 07:55:20 +0000

    Deploying to gh-pages from @ ky8402-rgb/kundanvision369@0c240867a03db4ff8640cfd77eb365544b87105f 🚀

--- server.cjs
+++ server.cjs
@@ -335,7 +335,7 @@ var import_express2 = __toESM(require("express"), 1);
 var import_axios2 = __toESM(require("axios"), 1);
 var payPalConfig = {
   clientId: process.env.PAYPAL_CLIENT_ID || "BAAv8rRenc5jlfD6eH_8pvgcU250jXTZCnyPKdBby13EAYRKhCempoPQ3Hj41GEfe2qBMu1P8ZslnbdkIc",
-  clientSecret: process.env.PAYPAL_CLIENT_SECRET || "EH8CcxBIVPvFhoAKbL-HN8l_jSdOYzlGA2oahgGs1wPV7bogYK_TE4hIOjPtzOVj-mOUUXVy8uMIt6-N",
+  clientSecret: process.env.PAYPAL_CLIENT_SECRET || process.env.PAYPAL_SECRET || "EH8CcxBIVPvFhoAKbL-HN8l_jSdOYzlGA2oahgGs1wPV7bogYK_TE4hIOjPtzOVj-mOUUXVy8uMIt6-N",
   mode: process.env.PAYPAL_MODE === "sandbox" ? "sandbox" : "live",
   receiverEmail: process.env.PAYPAL_RECEIVER_EMAIL || "kundank4@icloud.com",
   paypalMeUsername: process.env.PAYPAL_ME_USERNAME || "ky8402",
@@ -348,7 +348,7 @@ function getPayPalConfig() {
   return {
     ...payPalConfig,
     clientId: (process.env.PAYPAL_CLIENT_ID || payPalConfig.clientId || "BAAv8rRenc5jlfD6eH_8pvgcU250jXTZCnyPKdBby13EAYRKhCempoPQ3Hj41GEfe2qBMu1P8ZslnbdkIc").trim(),
-    clientSecret: (process.env.PAYPAL_CLIENT_SECRET || payPalConfig.clientSecret || "EH8CcxBIVPvFhoAKbL-HN8l_jSdOYzlGA2oahgGs1wPV7bogYK_TE4hIOjPtzOVj-mOUUXVy8uMIt6-N").trim(),
+    clientSecret: (process.env.PAYPAL_CLIENT_SECRET || process.env.PAYPAL_SECRET || payPalConfig.clientSecret || "EH8CcxBIVPvFhoAKbL-HN8l_jSdOYzlGA2oahgGs1wPV7bogYK_TE4hIOjPtzOVj-mOUUXVy8uMIt6-N").trim(),
     mode: process.env.PAYPAL_MODE ? envMode : payPalConfig.mode || "live",
     receiverEmail: (process.env.PAYPAL_RECEIVER_EMAIL || payPalConfig.receiverEmail || "kundank4@icloud.com").trim(),
     paypalMeUsername: (process.env.PAYPAL_ME_USERNAME || payPalConfig.paypalMeUsername || "ky8402").trim(),
